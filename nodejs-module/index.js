0adb5c359203b4f21728c5396917487148b2c477
unknown <tr9836859@gmail.com>
2023-06-03 13:26:34 +0600

    chore:for the nodejs-module

--- nodejs-module/index.js
+++ nodejs-module/index.js
@@ -1,24 +1,30 @@
-// This is the commonjs.module import(node.js e alltime common.js ei import kora hoy):
+// // This is the commonjs.module import(node.js e alltime common.js ei import kora hoy):
 
-const arrayManipulation = require("./utils");
+const { reverseString } = require("./utils");
 
-console.log(
-  arrayManipulation.getUpperCaseOrLowerCase(
-    ["Tanvir", "rifat", "william", "Sentance"],
-    "lowercase"
-  )
-);
+// const arrayManipulation = require("./utils");
 
-console.log(
-  arrayManipulation.getUpperCaseOrLowerCase(
-    ["Tanvir", "rifat", "william", "Sentance"],
-    "uppercase"
-  )
-);
+// console.log(
+//   arrayManipulation.getUpperCaseOrLowerCase(
+//     ["Tanvir", "rifat", "william", "Sentance"],
+//     "lowercase"
+//   )
+// );
 
-console.log(
-  arrayManipulation.getIndex(
-    ["Tanvir", "rifat", "william", "Sentance"],
-    "Sentance"
-  )
-);
+// console.log(
+//   arrayManipulation.getUpperCaseOrLowerCase(
+//     ["Tanvir", "rifat", "william", "Sentance"],
+//     "uppercase"
+//   )
+// );
+
+// console.log(
+//   arrayManipulation.getIndex(
+//     ["Tanvir", "rifat", "william", "Sentance"],
+//     "Sentance"
+//   )
+// );
+
+// taking a input:
+const str = process.argv[2];
+console.log(reverseString(str));
--- nodejs-module/utils.js
+++ nodejs-module/utils.js
@@ -1,17 +1,21 @@
-const arrayManipulation = {
-  getUpperCaseOrLowerCase: (myStringArr, Case) => {
-    let newArr = [];
-    for (const element of myStringArr) {
-      newArr.push(
-        Case === "uppercase" ? element.toUpperCase() : element.toLowerCase()
-      );
-    }
-    return newArr;
-  },
+// const arrayManipulation = {
+//   getUpperCaseOrLowerCase: (myStringArr, Case) => {
+//     let newArr = [];
+//     for (const element of myStringArr) {
+//       newArr.push(
+//         Case === "uppercase" ? element.toUpperCase() : element.toLowerCase()
+//       );
+//     }
+//     return newArr;
+//   },
 
-  getIndex: (myStringArr, element) => {
-    return myStringArr.indexOf(element);
-  },
+//   getIndex: (myStringArr, element) => {
+//     return myStringArr.indexOf(element);
+//   },
+// };
+
+// module.exports = arrayManipulation;
+
+exports.reverseString = (str) => {
+  return Array.from(str).reverse().join("");
 };
-
-module.exports = arrayManipulation;
